aa0d2aab82d7f1ae0a9da6743424c315ae29a6bc
InstructorMap <reguluskai3939@gmail.com>
2025-08-27 22:59:11 -0300

    Create database.js

---
+++ database.js
@@ -0,0 +1,25 @@
+// Este es tu archivo de base de datos.
+// Cada vez que generes nuevos carnets, deberás exportar los datos
+// de Formspree y agregarlos a esta lista en el formato correcto.
+
+const carnetDatabase = [
+  {
+    "titulo": "CARNET GRUPO COLBERT",
+    "cargo1": "INSTRUCTOR OFICIAL",
+    "cargo2": "EMERGENTOLOGIA",
+    "nombre": "JUAN PABLO (EJEMPLO)",
+    "dni": "27.337.188",
+    "matricula": "7188-123456",
+    "vence": "31/12/2026"
+  },
+  {
+    "titulo": "CARNET GRUPO COLBERT",
+    "cargo1": "ASISTENTE",
+    "cargo2": "PEDIATRIA",
+    "nombre": "MARIA GOMEZ (EJEMPLO)",
+    "dni": "30.123.456",
+    "matricula": "4591-654321",
+    "vence": "31/12/2025"
+  }
+  // Agrega aquí cada nuevo registro
+];
